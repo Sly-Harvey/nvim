791db97e4ed740402cdffc40219964163ffddb95
Sly-Harvey <mheharvey@gmail.com>
2023-09-16 18:47:31 +0100

    fixed nvim-tree auto open

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -19,11 +19,11 @@ local function open_nvim_tree(data)
       return
     end
   
-    -- open the tree, find the file but don't focus it
-    require("nvim-tree.api").tree.open()
+    -- open the tree and find the file
+    require("nvim-tree.api").tree.open({ find_file = true, focus = true, update_root = true })
   end
--- BufWinEnter to focal file
--- BufEnter to focus nvim-tree
+
+-- auto open nvim-tree
 vim.api.nvim_create_autocmd({"BufWinEnter"}, {
     group = vim.api.nvim_create_augroup("nvim_tree", { clear = true }),
     once = true,
