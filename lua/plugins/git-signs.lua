7236405d7989f1c3511d2f066b86683de683855c
Harvey Jacobs-Grant <mheharvey@gmail.com>
2023-12-01 23:24:11 +0000

    better file structure

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -1,1 +1,10 @@
-require('gitsigns').setup()
+return {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    ft = { "gitcommit", "diff" },
+    config = function() require('gitsigns').setup() end,
+    dependencies = {
+      'tpope/vim-fugitive',
+      'tpope/vim-rhubarb',
+    },
+}
