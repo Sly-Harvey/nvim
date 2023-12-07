99661357de2003f521e08e219ceb6ddd92df95e2
Harvey Jacobs-Grant <mheharvey@gmail.com>
2023-12-07 05:41:15 +0000

    improvements (:

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -5,50 +5,107 @@ require("core.keymaps")
 
 vim.cmd("colorscheme vscode")
 
+vim.api.nvim_create_augroup('bufcheck', { clear = true })
+
 -- disable semantic tokens
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      client.server_capabilities.semanticTokensProvider = nil
-    end,
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
 });
+
+-- auto update the highlight style on colorscheme change
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+  pattern = { "*" },
+  callback = function(ev)
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+  end
+})
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group    = 'bufcheck',
+  pattern  = '*',
+  callback = function()
+    if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.fn.setpos('.', vim.fn.getpos("'\""))
+      -- vim.cmd('normal zz') -- how do I center the buffer in a sane way??
+      vim.cmd('silent! foldopen')
+    end
+  end
+})
 
 -- Highlight when yanking
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('HighlightYank', {}),
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = vim.api.nvim_create_augroup('HighlightYank', {}),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = vim.api.nvim_create_augroup('resize_splits', { clear = true }),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
 })
 
 -- close with q
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("close-with-q", { clear = true }),
-    pattern = {
-      "PlenaryTestPopup",
-      "help",
-      "lspinfo",
-      "man",
-      "notify",
-      "qf",
-      "query",
-      "spectre_panel",
-      "startuptime",
-      "tsplayground",
-      "neotest-output",
-      "checkhealth",
-      "neotest-summary",
-      "neotest-output-panel",
-    },
-    callback = function(data)
-      vim.bo[data.buf].buflisted = false
-      vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = data.buf, silent = true })
-    end,
-  })
+  group = vim.api.nvim_create_augroup("close-with-q", { clear = true }),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "query",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+  },
+  callback = function(data)
+    vim.bo[data.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = data.buf, silent = true })
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("wrap_spell", { clear = true }),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
 
 -- auto open nvim-tree
 --vim.api.nvim_create_autocmd({"BufWinEnter"}, {
@@ -61,16 +118,16 @@ vim.api.nvim_create_autocmd("FileType", {
 --
 --      -- buffer is a real file on the disk
 --      local real_file = vim.fn.filereadable(data.file) == 1
---    
+--
 --      -- buffer is a [No Name]
 --      local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
---    
+--
 --      if not real_file and not no_name then
 --        return
---      elseif is_help then 
+--      elseif is_help then
 --        return
 --      end
---    
+--
 --      -- open the tree and find the file
 --      require("nvim-tree.api").tree.open({ find_file = true, focus = true, update_root = true })
 --    end
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,15 +2,21 @@ vim.g.mapleader = " "
 
 vim.opt.guicursor = ""
 
+-- Sync clipboards 
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
+vim.opt.fillchars = { eob = " " }
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.wrap = false
 
@@ -18,16 +24,22 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 vim.opt.isfname:append("@-@")
 
-vim.opt.lazyredraw = true
-vim.opt.updatetime = 50
+-- go to previous/next line with h,l,left arrow and right arrow
+--vim.opt.whichwrap:append "<>[]hl"
+
+vim.opt.lazyredraw = false
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
 --vim.opt.colorcolumn = "80"
