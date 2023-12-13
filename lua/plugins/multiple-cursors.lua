7fed82f5d8b9c6218c7affcc23dcd426b515c7a1
Harvey Jacobs-Grant <mheharvey@gmail.com>
2023-12-13 04:11:06 +0000

    added code actions

--- lua/plugins/multiple-cursors.lua
+++ lua/plugins/multiple-cursors.lua
@@ -6,8 +6,8 @@ return {
     },
     keys = {
         {"<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "i"}},
-        {"<C-j>", "<Cmd>MultipleCursorsAddDown<CR>"},
         {"<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "i"}},
+        {"<C-j>", "<Cmd>MultipleCursorsAddDown<CR>"},
         {"<C-k>", "<Cmd>MultipleCursorsAddUp<CR>"},
         {"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}},
         {"<Leader>a", "<Cmd>MultipleCursorsAddToWordUnderCursor<CR>", mode = {"n", "v"}},
