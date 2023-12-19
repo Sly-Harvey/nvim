748d0502937cf80821516d3424ba30055351d3ce
Harvey Jacobs-Grant <mheharvey@gmail.com>
2023-12-19 00:36:37 +0000

    chore: change vertical size for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,27 +1,33 @@
 return {
-    "akinsho/toggleterm.nvim",
-    event = { "BufReadPost", "UIEnter", "BufNewFile" },
-    cmd = { "ToggleTerm", "TermExec" },
-    opts = {
-      -- highlights = {
-      --   Normal = { link = "Normal" },
-      --   NormalNC = { link = "NormalNC" },
-      --   NormalFloat = { link = "NormalFloat" },
-      --   FloatBorder = { link = "FloatBorder" },
-      --   StatusLine = { link = "StatusLine" },
-      --   StatusLineNC = { link = "StatusLineNC" },
-      --   WinBar = { link = "WinBar" },
-      --   WinBarNC = { link = "WinBarNC" },
-      -- },
-      size = 10,
-      on_create = function()
-        vim.opt.foldcolumn = "0"
-        vim.opt.signcolumn = "no"
-      end,
-      start_in_insert = true,
-      open_mapping = [[<M-f>]],
-      shading_factor = 2,
-      direction = "horizontal", -- Options: horizontal, float, vertical, tab
-      float_opts = { border = "rounded", width = 155, height = 35 },
-    },
-  }
+  "akinsho/toggleterm.nvim",
+  event = { "BufReadPost", "UIEnter", "BufNewFile" },
+  cmd = { "ToggleTerm", "TermExec" },
+  opts = {
+    -- highlights = {
+    --   Normal = { link = "Normal" },
+    --   NormalNC = { link = "NormalNC" },
+    --   NormalFloat = { link = "NormalFloat" },
+    --   FloatBorder = { link = "FloatBorder" },
+    --   StatusLine = { link = "StatusLine" },
+    --   StatusLineNC = { link = "StatusLineNC" },
+    --   WinBar = { link = "WinBar" },
+    --   WinBarNC = { link = "WinBarNC" },
+    -- },
+    size = function(term)
+      if term.direction == "horizontal" or term.direction == "float" then
+        return 10
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    on_create = function()
+      vim.opt.foldcolumn = "0"
+      vim.opt.signcolumn = "no"
+    end,
+    start_in_insert = true,
+    open_mapping = [[<M-f>]],
+    shading_factor = 2,
+    direction = "float",   -- Options: horizontal, float, vertical, tab
+    float_opts = { border = "rounded", width = 155, height = 35 },
+  },
+}
