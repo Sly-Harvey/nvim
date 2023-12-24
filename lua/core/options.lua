d2359c7b3fbb016bef9386e96646db137b5939b1
Harvey Jacobs-Grant <mheharvey@gmail.com>
2023-12-24 18:25:44 +0000

    mods to telescope

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 
 -- Press <leader>cs to change theme!
-vim.g.colorscheme = "kanagawa" -- everforest, vscode, kanagawa, onedark, gruvbox catppuccin, rose-pine, etc.
+vim.g.colorscheme = "everforest" -- everforest, vscode, kanagawa, onedark, gruvbox catppuccin, rose-pine, etc.
 
 vim.g.everforest_transparent = false -- set transparency for everforest
 vim.g.auto_open_nvimtree = false
